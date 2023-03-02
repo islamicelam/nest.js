0dfbcb43897bb8d58c882d520a0ef5b26174e92c
islamicelam <islamovice@gmail.com>
2023-03-03 00:39:53 +0400

    Authentication

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,24 +1,30 @@
-import { Injectable } from '@nestjs/common';
-import {InjectModel} from "@nestjs/sequelize";
-import {User} from "./users.model";
-import {CreateUserDto} from "./dto/create-user.dto";
-import {RolesService} from "../roles/roles.service";
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/sequelize";
+import { User } from "./users.model";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { RolesService } from "../roles/roles.service";
 
 @Injectable()
 export class UsersService {
 
     constructor(@InjectModel(User) private userRepository: typeof User,
                 private roleService: RolesService) {}
-
     async createUser (dto: CreateUserDto) {
-        const user = await this.userRepository.create(dto);
+        console.log(dto);
+        // const user = await this.userRepository.create(dto);
+        const user = await this.userRepository.create(dto)
         const role = await this.roleService.getRoleByValue('USER');
+        console.log(role);
         await user.$set('roles', [role.id]);
+        user.roles = [role]
         return user;
     }
 
     async getAllUsers () {
-        const users = await this.userRepository.findAll({include: {all: true}});
-        return users;
+        return await this.userRepository.findAll({ include: { all: true } });
+    }
+
+    async getUsersByEmail (email: string) {
+        return await this.userRepository.findOne({ where: { email }, include: { all: true } });
     }
 }
