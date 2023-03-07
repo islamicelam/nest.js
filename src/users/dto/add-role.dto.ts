a159154f343db9287cc11446cd6facf1b14c8b17
islamicelam <islamovice@gmail.com>
2023-03-07 15:23:47 +0400

    Validation pipe

--- src/users/dto/add-role.dto.ts
+++ src/users/dto/add-role.dto.ts
@@ -1,5 +1,8 @@
+import { IsNumber, IsString } from "class-validator";
 
 export class AddRoleDto {
+  @IsString({message: "Should be string"})
   readonly value: string;
+  @IsNumber({}, {message: "Should be a number"})
   readonly userId: number;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,14 @@
 import {ApiProperty} from "@nestjs/swagger";
+import { IsEmail, IsString, Length } from "class-validator";
 
 export class CreateUserDto {
 
     @ApiProperty({example: "example@gmail.com", description: "Email"})
+    @IsString({message: "Should be string"})
+    @IsEmail({}, {message: "Wrong email"})
     readonly email: string
     @ApiProperty({example: "Aa1234!", description: "Password"})
+    @IsString({message: "Should be string"})
+    @Length(4, 16 , {message: "Should be 4-16 symbols"})
     readonly password: string
 }
